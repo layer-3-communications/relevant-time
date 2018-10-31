31662cc180665a72b077815ad120d8219e7ca37c
Zachary Churchill <zacharyachurchill@gmail.com>
2018-10-31 16:13:12 -0400

    relativizeAbsoluteTime (#1)

--- Data/RelevantTime.hs
+++ Data/RelevantTime.hs
@@ -9,13 +9,14 @@ module Data.RelevantTime
   , encodeRelevantTime
   , decodeRelevantTime
   , absolutizeRelevantTime
+  , relativizeAbsoluteTime
   ) where
 
 import Data.Text (Text)
 import Data.Aeson (ToJSON(..),FromJSON(..))
 import Data.Int (Int64)
 import Chronos.Types (Time)
-import Torsor (add,invert,scale)
+import Torsor (add,difference,invert,scale)
 import GHC.Generics (Generic)
 import qualified Chronos as CH
 import qualified Data.Text as T
@@ -63,11 +64,23 @@ decodeRelevantTime t = if T.null t
 absolutizeRelevantTime :: Time -> RelevantTime -> Time
 absolutizeRelevantTime now = \case
   RelevantTimeNow -> now
-  RelevantTimeNoon-> add (scale 12 CH.hour) (CH.dayToTimeMidnight (CH.timeToDayTruncate now))
+  RelevantTimeNoon -> add (scale 12 CH.hour) (CH.dayToTimeMidnight (CH.timeToDayTruncate now))
   RelevantTimeMidnight -> CH.dayToTimeMidnight (CH.timeToDayTruncate now)
   RelevantTimeMinute n -> add (invert (scale n CH.minute)) now
   RelevantTimeHour n -> add (invert (scale n CH.hour)) now
   RelevantTimeDay n -> add (invert (scale n CH.day)) now
+
+relativizeAbsoluteTime :: Time -> Time -> RelevantTime
+relativizeAbsoluteTime now abst
+  | absolutizeRelevantTime now RelevantTimeNoon == abst = RelevantTimeNoon
+  | absolutizeRelevantTime now RelevantTimeMidnight == abst = RelevantTimeMidnight
+  | (abs $ CH.getTimespan (now `difference` abst)) <  (CH.getTimespan CH.minute) = RelevantTimeNow
+  | (abs $ CH.getTimespan (now `difference` abst)) <= (CH.getTimespan CH.minute) * 59 = RelevantTimeMinute $ round $ (toFrac $ CH.getTime abst) / 60000000000
+  | (abs $ CH.getTimespan (now `difference` abst)) <= (CH.getTimespan CH.hour) * 23 = RelevantTimeHour $ round $ (toFrac $ CH.getTime abst) / 3600000000000
+  | otherwise = RelevantTimeDay $ round $ (toFrac $ CH.getTime abst) / 86400000000000
+  where
+  toFrac :: Int64 -> Double
+  toFrac = fromIntegral . abs
 
 readInt :: Text -> Maybe Int64
 readInt x = case TR.signed TR.decimal x of
